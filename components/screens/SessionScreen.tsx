51fa0a99113fcb63c5f1bb9e519df456a9f26c25
Prashant <prashan1837d@gmail.com>
2025-11-23 10:56:46 +0530

    Minor changes

---
+++ components/screens/SessionScreen.tsx
@@ -0,0 +1,22 @@
+import { useConversation } from "@elevenlabs/react-native";
+import { Text, View } from "react-native";
+
+export default function SessionScreen() {
+    const conversation = useConversation({
+        onConnect: () => console.log('Connected to conversation'),
+        onDisconnect: () => console.log('Disconnected from conversation'),
+        onMessage: (message) => console.log('Received message:', message),
+        onError: (error) => console.error('Conversation error:', error),
+        onModeChange: (mode) => console.log('Conversation mode changed:', mode),
+        onStatusChange: (prop) => console.log('Conversation status changed:', prop.status),
+        onCanSendFeedbackChange: (prop) =>
+            console.log('Can send feedback changed:', prop.canSendFeedback),
+        onUnhandledClientToolCall: (params) => console.log('Unhandled client tool call:', params),
+    });
+
+    return (
+        <View>
+            <Text>Session Screen</Text>
+        </View>
+    )
+}
