49d592741ed0df6458ca68b38074a7e52f061064
Prashant <prashan1837d@gmail.com>
2025-11-28 21:09:09 +0530

    feat: add ParallaxScrollView component with animated header and session details.

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -2,7 +2,7 @@ import { sessions } from "@/utils/sessions";
 import { Image } from "expo-image";
 import { PropsWithChildren } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Animated, { useAnimatedRef } from "react-native-reanimated";
+import Animated, { interpolate, useAnimatedRef, useAnimatedStyle, useScrollOffset } from "react-native-reanimated";
 import Button from "./Button";
 
 const blurhash = "LNF$b0%gNI9G_LRl-qRkIAIUxvoz";
@@ -11,18 +11,54 @@ const HEADER_HEIGHT = 400;
 export default function ParallaxScrollView({ children }: PropsWithChildren) {
     const todaySession = sessions[Math.floor(Math.random() * sessions.length)];
     const scrollRef = useAnimatedRef<Animated.ScrollView>();
+    const scrollOffset = useScrollOffset(scrollRef);
+
+    const headerAnimatedStyle = useAnimatedStyle(() => {
+
+        const translateY =
+            scrollOffset.value <= 0
+                ? interpolate(
+                    scrollOffset.value,
+                    [-HEADER_HEIGHT, 0],
+                    [-HEADER_HEIGHT / 2, 0]
+                )
+                : 0;
+
+        const scale =
+            scrollOffset.value <= 0
+                ? interpolate(
+                    scrollOffset.value,
+                    [-HEADER_HEIGHT, 0],
+                    [2, 1]
+                )
+                : 1;
+
+        return {
+            transform: [
+                {
+                    translateY,
+                },
+                {
+                    scale,
+                }
+            ],
+        };
+    });
 
     return (
         <View style={styles.container}>
             <Animated.ScrollView
                 showsVerticalScrollIndicator={false}
                 ref={scrollRef}
+                scrollEventThrottle={16}
             >
-                <Image
-                    source={todaySession.image}
-                    placeholder={blurhash}
-                    style={{ width: "100%", height: HEADER_HEIGHT }}
-                />
+                <Animated.View style={headerAnimatedStyle}>
+                    <Image
+                        source={todaySession.image}
+                        placeholder={blurhash}
+                        style={{ width: "100%", height: HEADER_HEIGHT }}
+                    />
+                </Animated.View>
                 <View style={styles.headerContainer}>
 
                     <View style={{ flex: 1 }} />
